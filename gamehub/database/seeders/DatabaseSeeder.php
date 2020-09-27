d335ad8153d2a91815a3381509770a14f4500bea
Qwinsie <qvdsluis@gmail.com>
2020-09-27 16:41:57 +0200

    Added Seeder for users migration

--- gamehub/database/seeders/DatabaseSeeder.php
+++ gamehub/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        DB::table('users')->insert([
+            'username' => Str::random(10),
+            'password' => Hash::make('password'),
+            'image' => Str::random(10),
+            'description' => Str::random(10),
+        ]);
     }
 }
