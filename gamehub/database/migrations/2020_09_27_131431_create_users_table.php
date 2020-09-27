50d598cbe0ab21b5230b6957798fe14c39c36655
Qwinsie <qvdsluis@gmail.com>
2020-09-27 16:39:43 +0200

    Deleted default user migration

--- gamehub/database/migrations/2020_09_27_131431_create_users_table.php
+++ gamehub/database/migrations/2020_09_27_131431_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('role_id')->nullable();
+            $table->string('image');
+            $table->text('description');
             $table->timestamps();
         });
     }
