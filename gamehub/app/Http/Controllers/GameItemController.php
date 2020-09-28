44ce916faff0fe2d6098714a4d100657b5b25d01
Qwinsie <qvdsluis@gmail.com>
2020-09-28 11:10:58 +0200

    Adding function index() and find() (does not work yet)

--- gamehub/app/Http/Controllers/GameItemController.php
+++ gamehub/app/Http/Controllers/GameItemController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
+use DB;
+use App\Models\User;
 
 class GameItemController extends Controller
 {
+    public function index()
+    {
+        $games = Game::orderBy()
 
+        return view('game-item.index',compact('games'));
+    }
+
+    public function find($gameid)
+    {
+        $game = User::where('gameid', $gameid)->firstOrFail();
+
+        return view('game-items.index', [
+            'game' => $game
+    ]);
+    }
 }
