df3356b48a6aaa6921c2fca5b5cb17028afbd876
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-28 22:54:12 +0100

    Error of db table name is fixed

--- gamehub/database/migrations/2020_03_27_131920_create_games_table.php
+++ gamehub/database/migrations/2020_03_27_131920_create_games_table.php
@@ -13,7 +13,7 @@ class CreateGamesTable extends Migration
      */
     public function up()
     {
-        Schema::create('gamehub', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('image');
