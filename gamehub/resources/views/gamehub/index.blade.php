501ab1cf276bee35ee3ee4addd0f493d7e9438da
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-29 21:35:25 +0100

    Owner of added game is now listed and clickable + nav is cleaner

--- gamehub/resources/views/gamehub/index.blade.php
+++ gamehub/resources/views/gamehub/index.blade.php
@@ -1,24 +1,15 @@
-@extends('layout.layout')
+@extends('layouts.layout')
+@extends('layouts.app')
 
 @section ('content')
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="content">
             <div class="title m-b-md">
-                GameHub
+                @if (Auth::check())
+                    Hi {{ Auth::user()->name }}
+                @else
+                    Welcome to GameHub
+                @endif
             </div>
 
             <div class="links">
@@ -26,27 +17,27 @@
                 <a href="{{route('about.index')}}">About Us</a>
             </div>
             <div>
-                    <table>
-                        <tr>
-                            <td>Image</td>
-                            <td>Name</td>
-                            <td>Year</td>
-                            <td>Company</td>
-                            <td>User</td>
-                            <td>Detail</td>
-                        </tr>
-                        @foreach($games as $game)
-                            @php /** @var App\Models\Game $game */ @endphp
-                        <tr>
-                            <td><img src="{{$game->image}}" alt=""></td>
-                            <td>{{$game->name}}</td>
-                            <td>{{$game->year}}</td>
-                            <td>{{$game->company}}</td>
-                            <td>{{$game->user->username}}</td>
-                            <td><a href="{{ $game->path() }}">Detail</a></td>
-                        </tr>
-                        @endforeach
-                    </table>
+                <table>
+                    <tr>
+                        <td>Image</td>
+                        <td>Name</td>
+                        <td>Year</td>
+                        <td>Company</td>
+                        <td>User</td>
+                        <td>Detail</td>
+                    </tr>
+                    @foreach($games as $game)
+                        @php /** @var App\Models\Game $game */ @endphp
+                    <tr>
+                        <td><img src="{{$game->image}}" alt=""></td>
+                        <td>{{$game->name}}</td>
+                        <td>{{$game->year}}</td>
+                        <td>{{$game->company}}</td>
+                        <td><a href="{{ $game->user->path()}}">{{$game->user->name}}</a></td>
+                        <td><a href="{{ $game->path() }}">Detail</a></td>
+                    </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
