38e4ec449e29d57dbf0cb0aceab4fac6219bbc52
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-29 21:37:09 +0100

    Owner of added game is now listed in the details view

--- gamehub/resources/views/gamehub/show.blade.php
+++ gamehub/resources/views/gamehub/show.blade.php
@@ -1,21 +1,8 @@
-@extends('layout.layout')
+@extends('layouts.layout')
+@extends('layouts.app')
 
 @section ('content')
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="content">
             <div class="title m-b-md">
                 {{$game->name}}
@@ -27,11 +14,11 @@
             <div>
                 <table>
                     <tr>
-                        <td><img src="{{$game->image}}" alt=""></td>
+                        <td><img src="{{$game->image}}" alt="Profile picture"></td>
                         <td>{{$game->name}}</td>
                         <td>{{$game->year}}</td>
                         <td>{{$game->company}}</td>
-                        <td>{{$game->user_id}}</td>
+                        <td>{{$game->user->name}}</td>
                         <td><a href="edit/{{$game->id}}">Edit</a></td>
                     </tr>
                 </table>
