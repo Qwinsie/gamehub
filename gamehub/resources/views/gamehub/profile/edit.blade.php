2bfe43ddf1b69b410a4a023daab1501281745b84
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-30 14:50:07 +0100

    Working on edit view for users profile

---
+++ gamehub/resources/views/gamehub/profile/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.layout')
+@extends('layouts.app')
+
+@section ('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Edit your profile image
+            </div>
+            <div class="container">
+                <div class="panel panel-primary">
+                    <div class="panel-heading"><h2>Laravel 5.7 image upload example - HDTuto.com</h2></div>
+                    <div class="panel-body">
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success alert-block">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                                <strong>{{ $message }}</strong>
+                            </div>
+                            <img src="images/{{ Session::get('image') }}">
+                        @endif
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('profile.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="file" name="image" class="form-control">
+                                </div>
+                                <div class="col-md-6">
+                                    <button type="submit" class="btn btn-success">Upload</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
