32d84c4ea784c807ac057b9aa8aa5f648d5a012c
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-30 14:56:25 +0100

    Working on editing profiles and uploading images

--- gamehub/app/Http/Controllers/ProfileController.php
+++ gamehub/app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,30 @@ class ProfileController extends Controller
         if($profile === null){
             abort(404, "The profile you are looking for does not exist yet");
         }
-        return view('gamehub.profile.show',compact('profile'));
+        $games = Game::all()->where('user_id', $id);
+        if($games === null){
+            abort(404, "The game you are looking for does not exist yet");
+        }
+
+        return view('gamehub.profile.show', compact('profile'), compact('games'));
+    }
+
+    public function edit()
+    {
+        return view('gamehub.profile.edit');
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        $imageName = time().'.'.request()->image->getClientOriginalExtension();
+
+        request()->image->move(public_path('images'), $imageName);
+
+        return back()
+            ->with('success','You have successfully upload image.')
+            ->with('image',$imageName);
     }
 }
