6e72540cf3cd5ee58429a62973d0c0f8ae6646b6
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-30 14:57:08 +0100

    Users profile now include games added by that specific user

--- gamehub/resources/views/gamehub/profile/show.blade.php
+++ gamehub/resources/views/gamehub/profile/show.blade.php
@@ -5,11 +5,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                {{$profile->name}}
-            </div>
-
-            <div class="links">
-                <a href="{{route('gamehub.index')}}">Homepage</a>
+                {{$profile->name."'s Profile"}}
             </div>
             <div>
                 <table>
@@ -17,8 +13,35 @@
                         <td><img src="{{$profile->image}}" alt="Profile picture"></td>
                         <td>{{$profile->name}}</td>
                         <td>{{$profile->email}}</td>
-                        <td><a href="edit/{{$profile->id}}">Edit</a></td>
+                        <td><a href="{{$profile->id}}/edit">Edit</a></td>
                     </tr>
+                </table>
+            </div>
+
+            <div>
+                <a class="btn btn-primary" href="{{route('game.create')}}">Add a new Game to the list</a>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <td>Image</td>
+                        <td>Name</td>
+                        <td>Year</td>
+                        <td>Company</td>
+                        <td>User</td>
+                        <td>Detail</td>
+                    </tr>
+                    @foreach($games as $game)
+                        @php /** @var App\Models\Game $game */ @endphp
+                        <tr>
+                            <td><img src="{{$game->image}}" alt=""></td>
+                            <td>{{$game->name}}</td>
+                            <td>{{$game->year}}</td>
+                            <td>{{$game->company}}</td>
+                            <td><a href="{{ $game->user->path()}}">{{$game->user->name}}</a></td>
+                            <td><a href="{{ $game->path() }}">Detail</a></td>
+                        </tr>
+                    @endforeach
                 </table>
             </div>
         </div>
