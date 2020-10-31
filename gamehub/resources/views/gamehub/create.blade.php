2fcba8aba51771bcd39b2c886de933a0f9e35016
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 00:42:09 +0100

    You can now add images to your games

--- gamehub/resources/views/gamehub/create.blade.php
+++ gamehub/resources/views/gamehub/create.blade.php
@@ -7,60 +7,72 @@
             <div class="title m-b-md">
                 Add a game to your list!
             </div>
-            <div>
-                <form method="post" action="{{ route('game.store') }}">
+            <div class="container">
+                <div class="panel panel-primary">
+                    <div class="panel-heading"><h6>Fill in the fields</h6></div>
+                <form method="post" action="{{ route('game.store') }}" enctype="multipart/form-data">
                     @csrf
                         <input type="hidden"
                                class="form-control"
-                               value="{{Auth::user()->id}}"
+                               value="{{ Auth::user()->id }}"
                                id="user_id"
                                name="user_id"/>
                         @if($errors->has('user_id'))
                             <span class="alert-danger form-check-inline">{{$errors->first('user_id')}}</span>
                         @endif
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text"
-                                class="form-control"
-                                required
-                                id="name"
-                                name="name"
-                                value="{{ old('name') }}"/>
-                        @if($errors->has('name'))
-                            <span class="alert-danger form-check-inline">{{$errors->first('name')}}</span>
-                        @endif
+                    <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('name') }}</label>
+                        <div class="col-md-6">
+                            <input type="text"
+                                    class="form-control"
+                                    required
+                                    id="name"
+                                    name="name"
+                                    value="{{ old('name') }}"/>
+                            @if($errors->has('name'))
+                                <span class="alert-danger form-check-inline">{{$errors->first('name')}}</span>
+                            @endif
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="year">Year:</label>
-                        <input type="text"
-                               class="form-control"
-                               required
-                               id="year"
-                               name="year"
-                               value="{{ old('year') }}"/>
-                        @if($errors->has('year'))
-                            <span class="alert-danger form-check-inline">{{$errors->first('year')}}</span>
-                        @endif
+                    <div class="form-group row">
+                        <label for="year" class="col-md-4 col-form-label text-md-right">{{ __('year') }}</label>
+                        <div class="col-md-6">
+                            <input type="text"
+                                   class="form-control"
+                                   required
+                                   id="year"
+                                   name="year"
+                                   value="{{ old('year') }}"/>
+                            @if($errors->has('year'))
+                                <span class="alert-danger form-check-inline">{{$errors->first('year')}}</span>
+                            @endif
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="company">Company:</label>
-                        <input type="text"
-                               class="form-control"
-                               required id="company"
-                               name="company"
-                               value="{{ old('company') }}"/>
-                        @if($errors->has('company'))
-                            <span class="alert-danger form-check-inline">{{$errors->first('company')}}</span>
-                        @endif
+                    <div class="form-group row">
+                        <label for="company" class="col-md-4 col-form-label text-md-right">{{ __('company') }}</label>
+                        <div class="col-md-6">
+                            <input type="text"
+                                   class="form-control"
+                                   required id="company"
+                                   name="company"
+                                   value="{{ old('company') }}"/>
+                            @if($errors->has('company'))
+                                <span class="alert-danger form-check-inline">{{$errors->first('company')}}</span>
+                            @endif
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="image">Image URL:</label>
-                        <input type="text"
-                               class="form-control"
-                               id="image"
-                               name="image"/>
+                    <div class="form-group row">
+                        <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+                        <div class="col-md-6">
+                            <input type="file" class="" name="image"/>
+                            @if($errors->has('image'))
+                                <span class="alert-danger form-check-inline">{{$errors->first('image')}}</span>
+                            @endif
+                        </div>
                     </div>
-                    <button type="submit" class="btn-primary btn-block">Save Game</button>
+                    <div class="form-group row">
+                        <button type="submit" class="btn-primary btn-block">Save Game</button>
+                    </div>
                 </form>
             </div>
         </div>
