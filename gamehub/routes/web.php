446bbe9b1b8f493b3bd653d6735eb13a182ae083
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 00:49:02 +0100

    Added more routings

--- gamehub/routes/web.php
+++ gamehub/routes/web.php
@@ -22,20 +22,22 @@ Route::prefix('gamehub')->group(function(){
     Route::name('game.')->middleware('auth')->group(function(){
         Route::get('create', [App\Http\Controllers\GameController::class, 'create'])->name('create');
         Route::post('store', [App\Http\Controllers\GameController::class, 'store'])->name('store');
-        Route::post('{game}/update', [App\Http\Controllers\GameController::class, 'update'])->name('update');
         Route::get('{game}', [App\Http\Controllers\GameController::class, 'show'])->name('show');
+        Route::post('{game}/update', [App\Http\Controllers\GameController::class, 'update'])->name('update');
         Route::get('{game}/edit', [App\Http\Controllers\GameController::class, 'edit'])->name('edit');
         Route::get('{game}/delete', [App\Http\Controllers\GameController::class, 'delete'])->name('delete');
-
+        Route::post( '/games/{game}/like', [App\Http\Controllers\GameController::class, 'storeLike'])->name('store.like');
+        Route::delete( '/games/{game}/like', [App\Http\Controllers\GameController::class, 'destroy'])->name('store.like');
     });
 
 });
 Route::prefix('gamehub/profile')->name('profile.')->group(function(){
-    Route::get('edit/{id}', [App\Http\Controllers\ProfileController::class, 'edit'])->middleware('auth')->name('edit');
-    Route::post('store', [App\Http\Controllers\ProfileController::class, 'store'])->middleware('auth')->name('store');
-    Route::get('{id}', [App\Http\Controllers\ProfileController::class, 'show'])->name('show');
+    Route::get('{profile}/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->middleware('auth')->name('edit');
+    Route::post('{profile}/update', [App\Http\Controllers\ProfileController::class, 'update'])->middleware('auth')->name('update');
+    Route::get('{profile}', [App\Http\Controllers\ProfileController::class, 'show'])->name('show');
 
 });
+
 
 Route::get('about-us', [App\Http\Controllers\AboutController::class, 'index'])->name('about.index');
 
