cdfc33b6243b75355121b513214eb5e9e1ef9dbd
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 23:31:43 +0100

    Deleted comments

--- gamehub/app/Models/Preference.php
+++ gamehub/app/Models/Preference.php
@@ -34,60 +34,16 @@ class Preference extends Model
 
     protected $guarded = [];
 
-    public function scopeWithLikes(Builder $query)
-    {
-        $query->leftJoinSub(
-            'select game_id, sum(preference) likes, sum(!preference) dislikes from preferences group by game_id',
-            'likes',
-            'likes.game_id',
-            'games_id'
-        );
-    }
-
-    public function like($user = null, $liked = true)
-    {
-        $this->likes()->updateOrCreate(
-            [
-                'user_id' => $user ? $user->id : auth()->id(),
-            ],
-            [
-                'liked' => $liked,
-            ]
-
-        );
-    }
-
-    public function dislike($user = null)
-    {
-        return $this->like($user, false);
-    }
-
-    public function isLikedBy(User $user)
-    {
-        return (bool) $user->likes
-            ->where('game_id', $this->id)
-            ->where('liked', true)
-            ->count();
-    }
-
-    public function isDisLikedBy(User $user)
-    {
-        return (bool) $user->likes
-            ->where('game_id', $this->id)
-            ->where('liked', false)
-            ->count();
-    }
-
     public function likes()
     {
         return $this->hasMany(Like::class);
     }
 
-
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function game()
     {
         return $this->belongsTo(Game::class);
