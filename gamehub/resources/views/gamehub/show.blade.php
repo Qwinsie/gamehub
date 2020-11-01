cedc785f330bba6b1cab652865beaa62c6e41781
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 23:33:19 +0100

    Details page looks nicer

--- gamehub/resources/views/gamehub/show.blade.php
+++ gamehub/resources/views/gamehub/show.blade.php
@@ -6,17 +6,19 @@
         <div class="content">
             <div class="title m-b-md">
                 {{$game->name}}
+                <a class="btn btn-primary" href="{{ url()->previous() }}">Back</a>
+                @can('edit-game', $game)
+                    <a class="btn btn-primary" href="{{ route('game.edit', $game->id)}}">Edit</a>
+                    <a class="btn btn-primary" href="{{ route('game.delete', $game->id)}}">Delete</a>
+                @endcan
             </div>
+            Created by: <a href="{{ $game->user->path()}}">{{$game->user->name}}</a>
             <div>
-                <img src="{{URL("/images/games/$game->image")}}" alt="{{$game->name}}" >
-                <div>{{$game->name}}</div>
+                <img class="m-3" src="{{URL("/images/games/$game->image")}}" alt="{{$game->name}}" width="200" height="200">
+                <div>
                 {{$game->year}}
                 {{$game->company}}
-                <a href="{{ $game->user->path()}}">{{$game->user->name}}</a>
-                @can('edit-game', $game)
-                <a href="{{ route('game.edit', $game->id)}}">Edit</a>
-                <a href="{{ route('game.delete', $game->id)}}">Delete</a>
-                @endcan
+                </div>
             </div>
         </div>
     </div>
