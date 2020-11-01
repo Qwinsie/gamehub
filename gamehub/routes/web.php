eee80ad30883b8f1da47b46d7dec2b84881836ad
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 23:34:34 +0100

    Added route authorization based on Gate

--- gamehub/routes/web.php
+++ gamehub/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('', [App\Http\Controllers\GameController::class, 'index'])->name('gamehub.index');
 
+Route::any ( '/search', [\App\Http\Controllers\SearchController::class, 'search'])->name('search');
+
 Route::prefix('gamehub')->group(function(){
 
     Route::get('', [App\Http\Controllers\GameController::class, 'index'])->name('gamehub.index');
@@ -23,17 +26,17 @@ Route::prefix('gamehub')->group(function(){
         Route::get('create', [App\Http\Controllers\GameController::class, 'create'])->name('create');
         Route::post('store', [App\Http\Controllers\GameController::class, 'store'])->name('store');
         Route::get('{game}', [App\Http\Controllers\GameController::class, 'show'])->name('show');
-        Route::post('{game}/update', [App\Http\Controllers\GameController::class, 'update'])->name('update');
-        Route::get('{game}/edit', [App\Http\Controllers\GameController::class, 'edit'])->name('edit');
-        Route::get('{game}/delete', [App\Http\Controllers\GameController::class, 'delete'])->name('delete');
-        Route::post( '/games/{game}/like', [App\Http\Controllers\GameController::class, 'storeLike'])->name('store.like');
-        Route::delete( '/games/{game}/like', [App\Http\Controllers\GameController::class, 'destroy'])->name('store.like');
+        Route::post('{game}/update', [App\Http\Controllers\GameController::class, 'update'])->middleware('can:edit-game,game')->name('update');
+        Route::get('{game}/edit', [App\Http\Controllers\GameController::class, 'edit'])->middleware('can:edit-game,game')->name('edit');
+        Route::get('{game}/delete', [App\Http\Controllers\GameController::class, 'delete'])->middleware('can:edit-game,game')->name('delete');
+        Route::post( '/games/{game}/like', [App\Http\Controllers\GameLikesController::class, 'storeLike'])->name('store.like');
+        Route::delete( '/games/{game}/like', [App\Http\Controllers\GameLikesController::class, 'destroy'])->name('store.like');
     });
 
 });
 Route::prefix('gamehub/profile')->name('profile.')->group(function(){
-    Route::get('{profile}/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->middleware('auth')->name('edit');
-    Route::post('{profile}/update', [App\Http\Controllers\ProfileController::class, 'update'])->middleware('auth')->name('update');
+    Route::get('{profile}/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->middleware('can:edit-profile,profile')->name('edit');
+    Route::post('{profile}/update', [App\Http\Controllers\ProfileController::class, 'update'])->middleware('can:edit-profile,profile')->name('update');
     Route::get('{profile}', [App\Http\Controllers\ProfileController::class, 'show'])->name('show');
 
 });
