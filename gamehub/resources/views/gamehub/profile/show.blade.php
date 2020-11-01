b9fa277f1c70b1ea629bab7785041eda2a98ae61
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-01 23:32:54 +0100

    Personal information is now hidden and page looks nicer

--- gamehub/resources/views/gamehub/profile/show.blade.php
+++ gamehub/resources/views/gamehub/profile/show.blade.php
@@ -5,44 +5,47 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                {{$profile->name."'s Profile"}}
-            </div>
-            <div>
                 @if($profile->image)
                     <img src="{{URL("/images/$profile->image")}}" alt="Profile picture" width="200" height="200">
                 @endif
-                    {{$profile->name}}
-                    {{$profile->email}}
-                    {{$profile->description}}
-                    @can('edit-profile', $profile)
-                    <a href="{{route('profile.edit', $profile->id)}}">Edit</a>
-                    @endcan
-            </div>
-
-            <div>
-                <a class="btn btn-primary" href="{{route('game.create')}}">Add a new Game to the list</a>
+                {{$profile->name."'s Profile"}}
+                @can('edit-profile', $profile)
+                    <a class="btn btn-primary" href="{{route('profile.edit', $profile->id)}}">Edit</a>
+                @endcan
             </div>
             <div>
-                <table>
-                    <tr>
-                        <td>Image</td>
-                        <td>Name</td>
-                        <td>Year</td>
-                        <td>Company</td>
-                        <td>User</td>
-                        <td>Detail</td>
-                    </tr>
-                    @foreach($games as $game)
-                        @php /** @var App\Models\Game $game */ @endphp
+                {{$profile->description}}
+            </div>
+            <div>
+                @can('edit-profile', $profile)
+                <a class="btn btn-primary m-3" href="{{route('game.create')}}">Add a new Game to your list</a>
+                @endcan
+            </div>
+            <div>
+                <table class="table table-striped">
+                    <thead>
                         <tr>
-                            <td><img src="{{URL("/images/games/$game->image")}}" alt="{{$game->name}}" width="100" height="100"></td>
-                            <td>{{$game->name}}</td>
-                            <td>{{$game->year}}</td>
-                            <td>{{$game->company}}</td>
-                            <td><a href="{{ $game->user->path()}}">{{$game->user->name}}</a></td>
-                            <td><a href="{{ $game->path() }}">Detail</a></td>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Year</th>
+                            <th>Company</th>
+                            <th>User</th>
+                            <th>Detail</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach($games as $game)
+                            @php /** @var App\Models\Game $game */ @endphp
+                            <tr>
+                                <td><a href="{{ $game->path() }}"><img src="{{URL("/images/games/$game->image")}}" alt="{{$game->name}}" width="100" height="100"></td>
+                                <td>{{$game->name}}</td>
+                                <td>{{$game->year}}</td>
+                                <td>{{$game->company}}</td>
+                                <td><a href="{{ $game->user->path()}}">{{$game->user->name}}</a></td>
+                                <td><a class="btn btn-primary" href="{{ $game->path() }}">Detail</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
